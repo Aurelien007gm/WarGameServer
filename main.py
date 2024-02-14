8d58a9103538ebec417a466814fd0ef933003181
Aurelien <au.louesdon@gmail.com>
2024-02-14 16:08:56 +0100

    test

--- main.py
+++ main.py
@@ -12,5 +12,12 @@ def index():
 def test():
     return "Hello, ceci est un autre test."
 
+
+@app.function_name(name="HttpTrigger1")
+@app.route(route="req")
+def main(req):
+    user = req.params.get("user")
+    return f"Hello, {user}!"
+
 if __name__ == "__main__":
     app.run()
